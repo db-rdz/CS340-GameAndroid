59da697ccf2ae28350f20aa1ab0fe2165fc64128
Joshua Olson <0joshua.olson1@gmail.com>
2017-02-06 16:04:52 -0700

    Create AddGameToServerCommand.java

---
+++ app/src/main/java/tickettoride/ICommand/AddGameToServerCommand.java
@@ -0,0 +1,16 @@
+package tickettoride;
+import tickettoride.Server.Game;
+import tickettoride.Server.ServerFacade;
+class AddGameToServerCommand implements ICommand{
+  private final Game game;
+  private final int int_authentication_code;
+  private AddGameToServerCommand(){}
+  public AddGameToServerCommand(Game g, int k){
+    game = g;
+    int_authentication_code = k;}
+
+  @Override
+  public void execute(){
+    ServerFacade.SINGLETON.addGame(game);}
+  public int getAuthenticationCode(){
+    return int_authentication_code;}}
