7fa0be3facaf9468e33687259e3e0056294b1eed
Joshua Olson <0joshua.olson1@gmail.com>
2017-02-10 11:13:08 -0700

    Commented how I think DeleteGameCommand works
    That is, use this for joinable, waiting, AND resumable games (Add*Game's take care of changed game type)

--- app/src/main/java/tickettoride/ICommand/DeleteGameCommand.java
+++ app/src/main/java/tickettoride/ICommand/DeleteGameCommand.java
@@ -1,6 +1,6 @@
 package tickettoride.ICommand;
 import tickettoride.Client.ClientFacade;
-public class DeleteGameCommand implements ICommand{
+public class DeleteGameCommand implements ICommand{ // Joinable/Waiting/Resumable (Add*Game's take care of changed game type)
   private final String str_game_id;
   private DeleteGameCommand(){}
   public DeleteGameCommand(String gameId){
