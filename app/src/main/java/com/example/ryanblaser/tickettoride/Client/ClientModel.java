bde2fd4afec544ea2889afa193dafb5159483a5c
Joshua Olson <0joshua.olson1@gmail.com>
2017-02-15 14:05:14 -0700

    Import LobbyFragment for observer registration

--- app/src/main/java/com/example/ryanblaser/tickettoride/Client/ClientModel.java
+++ app/src/main/java/com/example/ryanblaser/tickettoride/Client/ClientModel.java
@@ -1,5 +1,6 @@
 package com.example.ryanblaser.tickettoride.Client;
 
+import com.example.ryanblaser.tickettoride.GUI.LobbyFragment;
 import com.example.ryanblaser.tickettoride.Server.Game;
 import com.example.ryanblaser.tickettoride.UserInfo.User;
 import com.example.ryanblaser.tickettoride.UserInfo.Username;
