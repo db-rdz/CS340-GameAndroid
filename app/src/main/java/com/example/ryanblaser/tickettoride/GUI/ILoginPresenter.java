2039dcf246088f56c8d518c4f6cc1c425774efbd
Joshua Olson <0joshua.olson1@gmail.com>
2017-02-15 15:41:39 -0700

    Create ILoginPresenter.java

---
+++ app/src/main/java/com/example/ryanblaser/tickettoride/GUI/ILoginPresenter.java
@@ -0,0 +1,12 @@
+package com.example.ryanblaser.tickettoride.GUI;
+
+import com.example.ryanblaser.tickettoride.UserInfo.User;
+
+/**
+ * Created by 0joshuaolson1 on 2/15/17.
+ */
+
+interface ILoginPresenter {
+    public void login(User user);
+    public void register(User user);
+}
