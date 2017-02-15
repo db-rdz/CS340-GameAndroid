ff96059037d962954816d5948de98771d88b479f
Joshua Olson <0joshua.olson1@gmail.com>
2017-02-15 15:09:48 -0700

    LobbyPresenter -> ILobbyPresenter

--- app/src/main/java/com/example/ryanblaser/tickettoride/Client/ClientModel.java
+++ app/src/main/java/com/example/ryanblaser/tickettoride/Client/ClientModel.java
@@ -1,6 +1,6 @@
 package com.example.ryanblaser.tickettoride.Client;
 
-import com.example.ryanblaser.tickettoride.GUI.LobbyPresenter;
+import com.example.ryanblaser.tickettoride.GUI.ILobbyPresenter;
 import com.example.ryanblaser.tickettoride.Server.Game;
 import com.example.ryanblaser.tickettoride.UserInfo.User;
 import com.example.ryanblaser.tickettoride.UserInfo.Username;
@@ -21,7 +21,7 @@ class ClientModel{
     private Hashtable<String, Game> list_joinable, list_waiting, list_resumable;
     private Hashtable<String, GameType> hashtable_id_to_list;
 
-    private LobbyPresenter lobbyPresenter = null;
+    private ILobbyPresenter lobbyPresenter = null;
 
     public ClientModel(){
         list_joinable = new Hashtable<String, Game>();
@@ -30,7 +30,7 @@ class ClientModel{
         hashtable_id_to_list = new Hashtable<String, GameType>();
     }
 
-    public void attachLobbyObserver(LobbyPresenter p){
+    public void attachLobbyObserver(ILobbyPresenter p){
         lobbyPresenter = p;
     }
 
