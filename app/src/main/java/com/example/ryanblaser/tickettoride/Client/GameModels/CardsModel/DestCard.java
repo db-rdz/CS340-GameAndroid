ab5095420617e297e5d0a9d266e6126980d2b616
danrod <dbenjaminrdz@gmail.com>
2017-03-11 17:31:31 -0700

    Update DestCard and PlayerActionPresenter.java

--- app/src/main/java/com/example/ryanblaser/tickettoride/Client/GameModels/CardsModel/DestCard.java
+++ app/src/main/java/com/example/ryanblaser/tickettoride/Client/GameModels/CardsModel/DestCard.java
@@ -2,19 +2,16 @@ package com.example.ryanblaser.tickettoride.Client.GameModels.CardsModel;
 
 import android.util.Pair;
 
-import com.example.ryanblaser.tickettoride.Client.GameModels.CityModel.City;
-
 /**
  * Created by RyanBlaser on 2/28/17.
  */
 
 public class DestCard implements iDestCard {
-
-    public DestCard(City c1, City c2){
-        _destination = new Pair<>(c1, c2);
-    }
-
-    private Pair<City, City> _destination;
+    private int _cardType;
+    private Pair<String, String> _destination;
     private int points;
 
+    public DestCard(String c1, String c2){
+        _destination = new Pair<>(c1, c2);
+    }
 }
